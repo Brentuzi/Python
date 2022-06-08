51bb466501fa475c852ed76fb79ef1e29f5f8a15
Brentuzi <90348225+Brentuzi@users.noreply.github.com>
2022-06-08 20:37:27 +0300

    Add files via upload

---
+++ lesson4/main.py
@@ -0,0 +1,49 @@
+from random import randint
+import time
+#Ввод имен играющих
+igrok1 = input('Введите имя 1-го играющего ')
+igrok2 = input('Введите имя 2-го играющего ')
+
+k=1
+
+score1=0
+score2=0
+wins1=0
+wins2=0
+i=0
+#Моделирование бросания кубика первым играющим
+for i in range(5):
+    print('Раунд ', i+1)
+    print('Кубик бросает', igrok1)
+    n1 = randint(1, 6)
+    score1+=n1
+    time.sleep(0.5)
+
+    print('Выпало:', n1)
+    print('Кубик бросает', igrok2)
+    n2 = randint(1, 6)
+    score2+=n2
+    time.sleep(0.5)
+    print('Выпало:', n2)
+    i+=1
+    time.sleep(0.5)
+
+    if n1 > n2:
+     print('Выиграл', igrok1, ' ', n1)
+     wins1+=1
+    elif n1 < n2:
+     print('Выиграл', igrok2, ' ', n2)
+     wins2 += 1
+    else:
+     print('Ничья')
+     wins2 += 1
+     wins2 += 1
+    print('---------------')
+print(' ')
+#Определение результата (3 возможных варианта)
+if score1 > score2:
+ print('Выиграл', igrok1, ' Очки:', score1, ' Выйгрышных раундов ', wins1)
+elif n1 < n2:
+ print('Выиграл', igrok2, ' Очки:', score2, ' Выйгрышных раундов ', wins2)
+else:
+ print('Ничья')
